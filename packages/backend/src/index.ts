6de678678cd18d31d4adb1fd7ac9651ff28b0569
andrew <andrewnijmeh1@gmail.com>
2022-09-27 13:46:37 -0400

    backend (#4)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,39 @@
-console.log('Hello, world!');
+import { ApolloServer } from 'apollo-server-fastify';
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+import fastify, { FastifyInstance } from 'fastify';
+
+function fastifyAppClosePlugin(app: FastifyInstance): ApolloServerPlugin {
+  return {
+    async serverWillStart() {
+      return {
+        async drainServer() {
+          await app.close();
+        },
+      };
+    },
+  };
+}
+
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  const app = fastify();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      fastifyAppClosePlugin(app),
+      ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+
+  await server.start();
+  app.register(server.createHandler());
+  await app.listen(4000);
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
