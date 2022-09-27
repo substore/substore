6b6f90dc826d43b3faccf6e92aa07fd4efabbff1
Shiv <sshivvss@gmail.com>
2022-09-27 10:40:41 +0530

    feat: cleanup and improve frontend

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-	return <h1>substore</h1>;
+	return <div>
+			<Head>
+				<title>Substore</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="description" content="Coming soon..." />
+			</Head>
+			<a href="https://github.com/substoredb">👀</a>
+		</div>
 };
 
 export default Home;
