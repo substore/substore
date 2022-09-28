162f40864410d622ca1c50678aa0e6c4bd887bfe
Shiv <sshivvss@gmail.com>
2022-09-28 11:46:41 +0530

    feat: add landing navbar

---
+++ apps/frontend/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Red+Hat+Display:wght@700&display=swap" rel="stylesheet"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Wrapper from "../components/Wrapper";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
-	return <div>
+	return <Wrapper>
 			<Head>
 				<title>Substore</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<meta name="description" content="Coming soon..." />
+				<meta name="description" content="The all-in-one data store platform, built from the ground up." />
 			</Head>
-			<a href="https://github.com/substoredb">👀</a>
-		</div>
+			<Navbar/> 
+		</Wrapper>
 };
 
 export default Home;
