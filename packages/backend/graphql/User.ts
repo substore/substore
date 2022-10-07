d8624c672363669967b37900fbc0891bdd921384
andrew <andrewnijmeh1@gmail.com>
2022-10-07 19:18:09 -0400

    Update User.ts

--- packages/backend/graphql/User.ts
+++ packages/backend/graphql/User.ts
@@ -1,9 +1,9 @@
 import { objectType } from "nexus";
 
-export const User = objectType([
+export const User = objectType({
   name: "User",
   definition(t) {
-    t.int('id')
+    t.int('id'),
     t.string('username')
   },
-)}
+});
