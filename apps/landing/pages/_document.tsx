9f465b1fcfa51465f6791a7014e2aa5187917fc2
andrew <andrewnijmeh1@gmail.com>
2022-11-01 17:45:26 -0400

    replace property -> name

--- apps/landing/pages/_document.tsx
+++ apps/landing/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Red+Hat+Display:wght@700&display=swap" rel="stylesheet"/>
         <meta property="og:type" content="website"/>
-        <meta property="description" content="The cloud platform for data. Deploy, and manage your databases with ease."/>
+        <meta name="description" content="The cloud platform for data. Deploy, and manage your databases with ease."/>
         <meta property="og:title" content="Substore | The cloud platform for data." />
         <meta property="og:image" content="https://user-images.githubusercontent.com/59238070/199231127-4cb39582-07db-4e66-a2d9-a35f60058122.png" />
           <meta property="twitter:image" content="https://user-images.githubusercontent.com/59238070/199231127-4cb39582-07db-4e66-a2d9-a35f60058122.png" />
