fea751d92cc24e254fa7a24dec4624f98be60de9
andrew <andrewnijmeh1@gmail.com>
2022-11-01 09:14:07 -0400

    twitter meta

--- apps/landing/pages/_document.tsx
+++ apps/landing/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <meta name="description" content="The cloud platform for data. Deploy, and manage your databases with ease."/>
         <meta property="og:title" content="Substore | The cloud platform for data." />
         <meta property="og:image" content="https://user-images.githubusercontent.com/59238070/199231127-4cb39582-07db-4e66-a2d9-a35f60058122.png" />
+          <meta name="twitter:image" content="https://user-images.githubusercontent.com/59238070/199231127-4cb39582-07db-4e66-a2d9-a35f60058122.png" />
 
       </Head>
       <body>
