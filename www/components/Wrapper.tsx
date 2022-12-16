14046464095385bb3ff6ae0b8089627cdc92953c
andrew <andrewnijmeh1@gmail.com>
2022-12-16 16:01:05 -0500

    use astro for landing page

--- www/components/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Wrapper = ({ children }: { children: React.ReactNode }) => {
-    return <div className="bg-black h-screen w-screen overflow-y-auto">
-        {children}
-    </div>
-}
-
-export default Wrapper;
