60640f96566989748b338aa981ef46778cc7935b
andrew <andrewnijmeh1@gmail.com>
2023-01-04 18:13:22 -0500

    feat(packages): provisioner + backend, add schema for creating databases + add list of dbs to provisioner.ts

---
+++ packages/backend/src/graphql/schema/Store.ts
@@ -0,0 +1,103 @@
+import { list, enumType, mutationField, nonNull, objectType, queryField, stringArg } from "nexus";
+
+import { Store, StoreType } from "nexus-prisma";
+import { Context } from "../../context";
+import { randomBytes } from "crypto";
+import { deprovision, provision } from "../../../../provisioner/src";
+import { providers } from "../../provider";
+import { StoreType as PrismaStoreType } from "@prisma/client";
+
+export const store = objectType({
+    name: Store.$name,
+    description: Store.$description,
+    definition(t) {
+        t.field(Store.id);
+        t.field(Store.name);
+        t.field(Store.type);
+        t.field(Store.url);
+    },
+});
+
+export const storeType = enumType(StoreType);
+
+export const createStoreMutation = mutationField("createStore", {
+    type: store,
+    args: {
+        type: nonNull(storeType),
+    },
+
+    resolve: async (_, { type }: { type: PrismaStoreType }, ctx: Context) => {
+        if (!ctx.user) {
+            return;
+        }
+
+        const config = providers.get(type);
+        if (!config) {
+            return;
+        }
+
+        const password = randomBytes(9).toString("hex");
+        const storeApp = await provision({
+            image: config.image,
+            secrets: [
+                {
+                    key: config.password,
+                    value: password,
+                },
+            ],
+            disk: {
+                size: 1,
+                destination: config.data,
+            },
+            network: {
+                port: config.port,
+                internalPort: config.port,
+            },
+        });
+
+        const store = await ctx.db.store.create({
+            data: {
+              name: storeApp,
+              type: type,
+              url: `${config.protocol}://${config.username}:${password}@${storeApp}.fly.dev:${config.port}/${config.dbName}`,
+              userId: ctx.user.id,
+            },
+          });
+          return store;
+    },
+});
+
+export const deleteStoreMutation = mutationField("deleteStore", {
+    type: "Boolean",
+    args: {
+        name: nonNull(stringArg()),
+    },
+    resolve: async (_, { name }, ctx: Context) => {
+        if (!ctx.user) {
+            return;
+        }
+
+        try {
+            const store = await ctx.db.store.findFirst({
+                where: {
+                    name,
+                    userId: ctx.user.id,
+                },
+            });
+            if (!store) {
+                return;
+            }
+
+            await deprovision(name);
+            await ctx.db.store.delete({
+                where: {
+                  name,
+                },
+            });
+            return true;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    },
+});
