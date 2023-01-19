4a12d429972f42e598abe52baab5f42490ee4345
andrew <andrewnijmeh1@gmail.com>
2023-01-19 10:23:28 -0500

    feat(backend): add rest of database container path

--- packages/backend/src/provider.ts
+++ packages/backend/src/provider.ts
@@ -11,6 +11,42 @@ export const providers = new Map([
             protocol: "redis",
             username: "default",
             dbName: "",
-        }
-    ]
+        },
+    ],
+    [
+        "POSTGRESQL",
+        {
+          image: "ghcr.io/substore/postgres:15",
+          password_var: "POSTGRES_PASSWORD",
+          port: 5432,
+          data: "/var/lib/postgresql/data",
+          protocol: "redis",
+          username: "default",
+          dbName: "substore",
+        },
+    ],
+    [
+        "MYSQL",
+        {
+            image: "ghcr.io/substore/mysql:8",
+            password_var: "MYSQL_ROOT_PASSWORD",
+            port: 3306,
+            data: "/var/lib/mysql",
+            protocol: "mysql",
+            username: "root",
+            dbName: "substore",
+        },
+    ],
+    [
+        "MONGODB",
+        {
+            image: "ghcr.io/substore/mongo:latest",
+            password_var: "MONGO_INITDB_ROOT_PASSWORD",
+            port: 27017,
+            data: "/data/db",
+            protocol: "mongo",
+            username: "mongodb",
+            dbName: "",
+        },
+    ],
 ])
