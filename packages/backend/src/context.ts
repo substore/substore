822d459d9fc3ba8eba9c9668211a270336e85120
andrew <andrewnijmeh1@gmail.com>
2023-01-20 22:04:33 -0500

    update const to auto export

--- packages/backend/src/context.ts
+++ packages/backend/src/context.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { Request } from "express";
-import { prisma } from "./db";
+import { client } from "./db";
 
 export const getContext = (req: Request): Context => {
   return {
     user: req.user,
     req: req,
-    db: prisma,
+    db: client,
   };
 };
 
